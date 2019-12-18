70c7b8b3d4ea349deb7e355e8dfd63b41432fba2
Depala <jdepala@deloitte.com>
2019-12-18 17:50:27 +0530

    added port

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const port = process.env.PORT || 8080;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
